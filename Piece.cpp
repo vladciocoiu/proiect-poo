75d72b4218d0aef04ce03f1a2919a92f9dd54a37
Vlad Ciocoiu <vlad.014916@gmail.com>
2022-03-11 00:35:40 +0200

    Coded pseudo-legal moves generator for more pieces

--- Piece.cpp
+++ Piece.cpp
@@ -1,6 +1,8 @@
 #include "Piece.hpp"
 #include "Board.hpp"
 
+#include <unordered_map>
+
 // constructor de initializare
 Piece::Piece(const std::string& type_, int col_, int row_, bool color_) 
 : type{type_}, col{col_}, row{row_}, color{color_} {
@@ -36,20 +38,90 @@ std::ostream& operator<<(std::ostream& os, const Piece& pc) {
 
 // generates all pseudo-legal moves
 // (doesn't care about leaving the king in check)
-// only implemented for knights now, will probably make separate classes for each piece type later
+// will probably make separate classes for each piece type later
+// TODO: code captures and promotions
 std::vector<Move> Piece::generatePseudoLegalMoves(const Board& bd) {
+
+    // map for every move offset of every piece type
+    static const std::unordered_map<std::string, std::vector<std::pair<int, int>>> OFFSETS = {
+        {
+            "knight", 
+            {
+                {1, 2}, {-1, 2}, {1, -2}, {-1, -2},
+                {2, 1}, {-2, 1}, {2, -1}, {-2, -1}
+            }
+        },
+        {
+            "king", 
+            {
+                {1, 0}, {-1, 0}, {0, 1}, {0, -1},
+                {1, 1}, {-1, 1}, {1, -1}, {-1, -1}
+            }
+        },
+        {
+            "bishop", 
+            {
+                {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, 
+                {-1, 1}, {-2, 2}, {-3, 3}, {-4, 4}, {-5, 5}, {-6, 6}, {-7, 7}, 
+                {1, -1}, {2, -2}, {3, -3}, {4, -4}, {5, -5}, {6, -6}, {7, -7},
+                {-1, -1}, {-2, -2}, {-3, -3}, {-4, -4}, {-5, -5}, {-6, -6}, {-7, -7} 
+            }
+        },
+        {
+            "rook", 
+            {
+                {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0},
+                {-1, 0}, {-2, 0}, {-3, 0}, {-4, 0}, {-5, 0}, {-6, 0}, {-7, 0},
+                {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
+                {0, -1}, {0, -2}, {0, -3}, {0, -4}, {0, -5}, {0, -6}, {0, -7}
+            }
+        },
+        {
+            "queen", 
+            {
+                {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, {8, 8}, 
+                {-1, 1}, {-2, 2}, {-3, 3}, {-4, 4}, {-5, 5}, {-6, 6}, {-7, 7}, {-8, 8}, 
+                {1, -1}, {2, -2}, {3, -3}, {4, -4}, {5, -5}, {6, -6}, {7, -7}, {8, -8}, 
+                {-1, -1}, {-2, -2}, {-3, -3}, {-4, -4}, {-5, -5}, {-6, -6}, {-7, -7}, {-8, -8}, 
+                {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0},
+                {-1, 0}, {-2, 0}, {-3, 0}, {-4, 0}, {-5, 0}, {-6, 0}, {-7, 0},
+                {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
+                {0, -1}, {0, -2}, {0, -3}, {0, -4}, {0, -5}, {0, -6}, {0, -7}
+            },
+        }
+    };
+
     std::vector<Move> moves;
 
-    if (type == "knight") {
+    // get squares occupied by friendly pieces
+    std::set<std::pair<int, int>> occupiedSquares = bd.getOccupiedSquares(color);
 
-        // all directions the knight can move to
-        const std::pair<int, int> offsets[8] = {
-            {1, 2}, {-1, 2}, {1, -2}, {-1, -2},
-            {2, 1}, {-2, 1}, {2, -1}, {-2, -1}
-        };
+    if (type == "pawn") {
+        // TODO: code en passant
 
-        for (std::pair<int, int> offset: offsets) {
-            std::set<std::pair<int, int>> occupiedSquares = bd.getOccupiedSquares(color);
+        // white pawns move to bigger rows, and black pawns to smaller ones
+        static const int ROW_OFFSET = (color ? 1 : -1);
+
+        // needed for the 2 square move (pawns can only move 2 squares from their initial row)
+        static const int STARTING_ROW = (color ? 1 : 6);
+
+        // the row where the pawn can promote
+        static const int PROMOTION_ROW = (color ? 7 : 0);
+
+        // normal 1 square move
+        if(row + ROW_OFFSET >= 0 && row + ROW_OFFSET < 8 && !occupiedSquares.count({row + ROW_OFFSET, col})) {
+            moves.push_back(Move{col, row, col, row + ROW_OFFSET, color, false, false, false, false, ""});
+
+            // 2 square move
+            // can be possible only if normal move is possible
+            if(row == STARTING_ROW && row + 2 * ROW_OFFSET >= 0 
+            && row + 2 * ROW_OFFSET < 8 && !occupiedSquares.count({row + ROW_OFFSET, col})) {
+                moves.push_back(Move{col, row, col, row + 2 * ROW_OFFSET, color, false, false, false, false, ""});
+            }
+        }
+
+    } else {
+        for (std::pair<int, int> offset: OFFSETS.at(type)) {
 
             int newRow = row + offset.first;
             int newCol = col + offset.second;
@@ -61,11 +133,13 @@ std::vector<Move> Piece::generatePseudoLegalMoves(const Board& bd) {
             if(occupiedSquares.count({newRow, newCol})) continue;
 
             // make a new move that isn't a capture / castle / en passant / promotion
-            // with the corresponding start and end squares and color
+            // with the corresponding squares and color
             moves.push_back(Move{col, row, newCol, newRow, color, false, false, false, false, ""});
         }
 
     }
+    // TODO: code castle moves
+
     return moves;
 }
 
