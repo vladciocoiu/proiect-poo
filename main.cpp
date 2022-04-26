004d7d2ecb59b1f556c0de56349772dbc165499f
Vlad Ciocoiu <vlad.014916@gmail.com>
2022-04-26 12:11:33 +0300

    tried to fix a weird bug

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@
 #include "Queen.hpp"
 
 int main() {
-    Board bd;
+    Board* bd = new Board();
     Knight pc {Piece {1, 0, true}}; // white knight on B1
-    bd.addPiece(pc);
+    bd->addPiece(pc);
     std::cout << bd;    
 
     Move mv {1, 0, 0, 2, true, false, false, false, false, ""}; // the knight moves from B1 to A3
 
-    std::vector<Move> knightMoves = pc.generatePseudoLegalMoves(bd);
+    std::vector<Move> knightMoves = pc.generatePseudoLegalMoves(*bd);
     for (const Move& mov: knightMoves) std::cout << mov;
 
     std::cout << pc;
