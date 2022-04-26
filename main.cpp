5c6d665399eea503180de6dfc69797c25fc1afc8
Vlad Ciocoiu <vlad.014916@gmail.com>
2022-04-26 15:56:46 +0300

    At this point, I don't even know what I'm doing

--- main.cpp
+++ main.cpp
@@ -15,21 +15,20 @@
 #include "Queen.hpp"
 
 int main() {
-    std::shared_ptr<Board> bd (new Board());
+    std::shared_ptr<Board> bd(new Board());
     std::shared_ptr<Piece> pc(new Knight(1, 0, true));
-    // Piece* pc = new Knight(1, 0, true);
-    // Knight pc {Piece {1, 0, true}}; // white knight on B1
+    std::shared_ptr<Move> mv(new Move(1, 0, 0, 2, true, false, false, false, false, ""));
+
     bd->addPiece(*pc);
     std::cout << *bd;    
-
-    Move mv {1, 0, 0, 2, true, false, false, false, false, ""}; // the knight moves from B1 to A3
 
     std::vector<Move> knightMoves = std::dynamic_pointer_cast<Knight>(pc)->generatePseudoLegalMoves(*bd);
     for (const Move& mov: knightMoves) std::cout << mov;
 
     std::cout << *pc;
 
-    pc->makeMove(mv);
+    // the knight moves from B1 to A3
+    pc->makeMove(*mv);
 
     std::cout << *pc;
 
