4d5d3fe6aa526994e3c5101fc9b15e21a29be78a
Vlad Ciocoiu <vlad.014916@gmail.com>
2022-04-27 12:04:05 +0300

    Trying to make Ubuntu Clang shut up

--- AI.cpp
+++ AI.cpp
@@ -3,15 +3,13 @@
 // TODO: code the search function that searches the current board position up to a certain depth 
 // and returns a pair consisting of the evaluation and the best move
 std::pair<int, Move> AI::search(int depth, const Board &bd) {
-    int x = depth;
-    for(auto piece: bd.getPieces()) x += piece->getSquare().first;
-    return {x, Move{1, 0, 0, 2, true, false, false, false, false, ""}};
+    for(auto piece: bd.getPieces()) std::cout << *piece;
+    return {depth, Move{1, 0, 0, 2, true, false, false, false, false, ""}};
 }
 
 
 // TODO: code the evaluate function that evaluates the current position on the board and returns the score
 int AI::evaluate(const Board &bd) {
-    int x = 0;
-    for(auto piece: bd.getPieces()) x += piece->getSquare().first;
-    return x;
+    for(auto piece: bd.getPieces()) std::cout << *piece;
+    return 0;
 }
