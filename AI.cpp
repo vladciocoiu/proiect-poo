c02a217ed84d9fd626e5a84b9ff2be1eaae2498f
Vlad Ciocoiu <vlad.014916@gmail.com>
2022-04-27 11:54:46 +0300

    Used unused variables

--- AI.cpp
+++ AI.cpp
@@ -3,11 +3,15 @@
 // TODO: code the search function that searches the current board position up to a certain depth 
 // and returns a pair consisting of the evaluation and the best move
 std::pair<int, Move> AI::search(int depth, const Board &bd) {
-    return {0, Move{1, 0, 0, 2, true, false, false, false, false, ""}};
+    int x = depth;
+    for(auto piece: bd.getPieces()) x++;
+    return {x, Move{1, 0, 0, 2, true, false, false, false, false, ""}};
 }
 
 
 // TODO: code the evaluate function that evaluates the current position on the board and returns the score
 int AI::evaluate(const Board &bd) {
-    return 0;
+    int x = 0;
+    for(auto piece: bd.getPieces()) x++;
+    return x;
 }
